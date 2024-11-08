698de8ab826d5501202c289bed99bd4b3a85ea37
Gustavinho2C <00001103720454sp@al.educacao.sp.gov.br>
2024-11-08 11:28:37 -0300

    Add files via upload

---
+++ perguntas do gustavinho/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'qual o maior time do brasil?',
+    'O Santos fc '
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'emprendedorismo',
+    'marca de material esportivo mais valiosa do mundo?',
+    'Nike'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
